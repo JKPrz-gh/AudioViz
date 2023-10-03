2c81c14c63db148e6f768e0495609670115e4dfe
Jan K. Przybyszewski <jan@dbcon.eu>
2023-10-03 14:30:46 +0100

    Initial commit for main file

---
+++ main.py
@@ -0,0 +1,14 @@
+'''Main file for AudioViz Project.
+
+As this is intended to be released as a library,
+this file exists for testing purposes and will be
+removed from the final release.
+'''
+
+
+def main() -> None:
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
