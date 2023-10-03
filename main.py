5bb0c39bedf655439c6bc32af863c151ce5c2343
Jan K. Przybyszewski <jan@dbcon.eu>
2023-10-03 15:26:24 +0100

    feat: creating basic spectrograms with matplotlib

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 '''Main file for AudioViz Project.
 
 As this is intended to be released as a library,
@@ -5,9 +6,22 @@ this file exists for testing purposes and will be
 removed from the final release.
 '''
 
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+
 
 def main() -> None:
-    print("Hello World")
+    # Y is amplitude vector, sr is sample rate
+    y, sr = librosa.load(librosa.ex('trumpet'))
+
+    D = librosa.stft(y)  # STFT of y
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+
+    plt.figure()
+    librosa.display.specshow(S_db)
+    plt.colorbar()
+    plt.show()
 
 
 if __name__ == "__main__":
